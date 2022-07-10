1a5f9478d59d8d9cb2b5ce10dcce0d451008a978
Ales Cepek <cepek@gnu.org>
2022-07-10 17:04:20 +0200

    New release

--- lib/gnu_gama/version.cpp
+++ lib/gnu_gama/version.cpp
@@ -49,7 +49,7 @@ namespace GNU_gama {
    *
    * Checked by tests/gama-local/scripts/check_version.cpp
    */
-  std::string GNU_gama_version() { return "2.20"; }
+  std::string GNU_gama_version() { return "2.21"; }
 
   std::string GNU_gama_compiler()
   {
